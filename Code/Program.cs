1bacdbde615173cd8cc075699900cddce94a4902
Ekaterina <katya.kinal@gmail.com>
2022-09-16 13:42:25 +0300

    Добавлены изменения в проект

--- Code/Program.cs
+++ Code/Program.cs
@@ -1,36 +1,36 @@
-﻿    void GetArray(string[] arr)
-    {
+﻿void GetArray(string[] arr)
+{
     for (int i = 0; i < arr.Length; i++)
     {
-    Console.Write("Введите элемент массива: ");
-    arr[i] = Console.ReadLine();
+        Console.Write("Введите элемент массива: ");
+        arr[i] = Console.ReadLine();
     }
-    }
+}
 
-    void PrintArray(string[] arr)
-    {
+void PrintArray(string[] arr)
+{
 
     for (int i = 0; i < arr.Length; i++)
     {
-    Console.Write($"{arr[i]} ");
+        Console.Write($"{arr[i]} ");
     }
     Console.WriteLine();
-    }
+}
 
-    void GetNewArray(string[] newArr)
-    {
+void GetNewArray(string[] newArr)
+{
     for (int i = 0; i < newArr.Length; i++)
     {
-    if(newArr[i].Length <= 3 ) Console.Write($"{newArr[i]} ");
+        if (newArr[i].Length <= 3) Console.Write($"{newArr[i]} ");
     }
     Console.WriteLine();
-    }
+}
 
-    string[] array = new string [4];
-    GetArray(array);
-    Console.WriteLine("Исходный массив:");
-    PrintArray(array);
-    Console.WriteLine("Новый массив:");
-    GetNewArray(array);
+string[] array = new string[4];
+GetArray(array);
+Console.WriteLine("Исходный массив:");
+PrintArray(array);
+Console.WriteLine("Новый массив:");
+GetNewArray(array);
 
 
